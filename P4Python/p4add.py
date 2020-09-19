08e82bd0c12695562f165e02707a58e9d69e817c
p4misc <p4miscjp@gmail.com>
2020-09-19 10:14:40 +0900

    Update p4add.py so that we can repeat this script easily

--- P4Python/p4add.py
+++ P4Python/p4add.py
@@ -1,6 +1,7 @@
 from P4 import P4,P4Exception
 import os
 import re
+import stat
 import pathlib
 import config
 
@@ -8,6 +9,7 @@ p4 = P4()
 p4.port = config.P4PORT
 p4.user = config.P4USER
 p4.client = config.P4CLIENT
+p4.exception_level = 1
 
 try:
   p4.connect()
@@ -16,8 +18,16 @@ try:
   workspace = p4.fetch_workspace(p4.client)
 
   dummy_file = pathlib.Path(workspace._Root + '\\dummy_file.txt')
-  with dummy_file.open(mode='w') as file:
-    file.write('dummy file')
+  # Delete the file if it is in the local directory
+  if dummy_file.exists():
+    dummy_file.chmod(stat.S_IWUSR)
+    dummy_file.unlink()
+
+  # p4 obliterate -y the file if it is in the depot
+  if p4.run_files(str(dummy_file)):
+    p4.run_obliterate('-y', str(dummy_file))
+
+  dummy_file.write_text('add dummy file')
 
   # p4 add file
   p4.run_add(str(dummy_file))
