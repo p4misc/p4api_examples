c1b9f554937de869ed2cab1cd893a5848259100c
p4misc <p4miscjp@gmail.com>
2020-09-20 15:44:51 +0900

    Added p4copy.py

---
+++ P4Python/p4copy.py
@@ -0,0 +1,57 @@
+from P4 import P4,P4Exception
+import os
+import re
+import stat
+import pathlib
+import config
+
+p4 = P4()
+p4.port = config.P4PORT
+p4.user = config.P4USER
+p4.client = config.P4CLIENT
+p4.exception_level = 1
+
+try:
+  p4.connect()
+
+  # p4 workspace -o 
+  workspace = p4.fetch_workspace(p4.client)
+
+  dummy_file1 = pathlib.Path(workspace._Root + '\\dummy_file1.txt')
+  dummy_file2 = pathlib.Path(workspace._Root + '\\dummy_file2.txt')
+  # Delete the file if it is in the local directory
+  for dummy_file in [dummy_file1, dummy_file2]:
+    if dummy_file.exists():
+      dummy_file.chmod(stat.S_IWUSR)
+      dummy_file.unlink()
+
+    # p4 obliterate -y the file if it is in the depot
+    if p4.run_files(str(dummy_file)):
+      p4.run_obliterate('-y', str(dummy_file))
+
+  dummy_file1.write_text('add dummy file')
+ 
+  # p4 add the file
+  p4.run_add(str(dummy_file1))
+  default_change = p4.fetch_change()
+  default_change._description = 'add a new file'
+  print(default_change)
+ 
+  # p4 submit the file
+  submitted_change = p4.run_submit(default_change)
+  print(submitted_change)
+ 
+  # p4 copy the file
+  p4.run_copy(str(dummy_file1), str(dummy_file2))
+  default_change = p4.fetch_change()
+  default_change._description = 'branch an existing file'
+  print(default_change)
+
+  # p4 submit the file
+  submitted_change = p4.run_submit(default_change)
+  print(submitted_change)
+
+  p4.disconnect()
+except P4Exception:
+  for e in p4.errors:
+      print(e)
